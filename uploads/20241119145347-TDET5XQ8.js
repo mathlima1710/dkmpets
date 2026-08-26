5d5f09e832aebc5e1af40a2d77cf5b026238c057
Matheus Rafael <matheuslimasilva258@gmail.com>
2026-08-26 16:50:02 -0300

    Primeiro commit

---
+++ uploads/20241119145347-TDET5XQ8.js
@@ -0,0 +1,30 @@
+window.botpress.init({
+  "botId": "001fb3a8-443d-4555-97ba-e794d08f48c5",
+  "configuration": {
+    "composerPlaceholder": "Digite sua mensagem...",
+    "botName": "Assistente Virtual da DKM Pets",
+    "botAvatar": "https://files.bpcontent.cloud/2024/11/19/16/20241119163201-ZYC96T0T.jpeg",
+    "botDescription": "Respostas geradas automaticamente por IA",
+    "website": {
+      "title": "www.dkmpets.com.br",
+      "link": "www.dkmpets.com.br"
+    },
+    "email": {
+      "title": "contato@dkmpets.com.br",
+      "link": "contato@dkmpets.com.br"
+    },
+    "phone": {
+      "title": "+5511992715519",
+      "link": "+5511992715519"
+    },
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#fad91f",
+    "variant": "solid",
+    "themeMode": "light",
+    "fontFamily": "inter",
+    "radius": 1,
+    "allowFileUpload": false
+  },
+  "clientId": "e72f4d85-4b09-4cb6-9855-2667acdb7786"
+});
